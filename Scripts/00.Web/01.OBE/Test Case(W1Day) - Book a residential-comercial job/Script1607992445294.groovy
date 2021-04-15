c5d369d0641372d74ff999e898ee180e9f965d03
LakshmiO2e <78434884+LakshmiO2e@users.noreply.github.com>
2021-04-15 14:38:16 -0300

    Commititng W1D Latest

--- Scripts/00.Web/01.OBE/Test Case(W1Day) - Book a residential-comercial job/Script1607992445294.groovy
+++ Scripts/00.Web/01.OBE/Test Case(W1Day) - Book a residential-comercial job/Script1607992445294.groovy
@@ -15,7 +15,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser(url)
+WebUI.openBrowser('https://fsm-w1d-obe.pantheonsite.io/onlinebooking')
 
 WebUI.maximizeWindow()
 
--- Test Suites/Test Suite - Web - W1Day.ts
+++ Test Suites/Test Suite - Web - W1Day.ts
@@ -18,84 +18,75 @@
       <testCaseId>Test Cases/00.Web/01.OBE/Test Case(W1Day) - Book a residential-comercial job</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</id>
+         <id>a6828024-2064-409e-aa59-4f7899e1ee66</id>
          <iterationEntity>
             <iterationType>ALL</iterationType>
             <value></value>
          </iterationEntity>
          <testDataId>Data Files/Data - WEB/02.W1Day/01.W1Day01_02</testDataId>
       </testDataLink>
-      <testDataLink>
-         <combinationType>ONE</combinationType>
-         <id>ba233f4e-170f-4af8-b647-dddb48083458</id>
-         <iterationEntity>
-            <iterationType>ALL</iterationType>
-            <value></value>
-         </iterationEntity>
-         <testDataId>Data Files/Data - WEB/02.W1Day/00.Login</testDataId>
-      </testDataLink>
       <variableLink>
-         <testDataLinkId>ba233f4e-170f-4af8-b647-dddb48083458</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>url</value>
+         <testDataLinkId></testDataLinkId>
+         <type>SCRIPT_VARIABLE</type>
+         <value>null</value>
          <variableId>08f884b6-9ad7-4867-813d-a261fd10f5b6</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptZipCode</value>
          <variableId>4e412d5b-b5d2-4e07-909d-820692dae4b2</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>slctPropertyType</value>
          <variableId>5d457edd-ecda-4962-8a4e-d7d7c4a45783</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>slctWhatToPaint</value>
          <variableId>819a3f96-2261-4c30-aaaa-0ef009e7c429</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>slctConsultationPrefer</value>
          <variableId>e2617e21-ca17-48de-8f10-d896417051bd</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptFirstName</value>
          <variableId>2cdaed5c-8de1-44aa-85cb-674a58a853c6</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptLastName</value>
          <variableId>6a681c4c-68a7-4e29-943f-df469b9d013b</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptEmail</value>
          <variableId>92a1cc81-e85f-4494-bc2b-4d39dcd2510d</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptFullAddress</value>
          <variableId>328fbb6a-7020-47ea-bf44-70da11507bac</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>verifyPageConfirmation</value>
          <variableId>ab81d67f-b09c-45c2-83d1-2f9bf6d8318c</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>2fa2a787-e280-4999-8b2d-d0059e3dd9e8</testDataLinkId>
+         <testDataLinkId>a6828024-2064-409e-aa59-4f7899e1ee66</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>inptPhone</value>
          <variableId>608ab9ec-71be-4653-8f57-b28634d8e69c</variableId>
