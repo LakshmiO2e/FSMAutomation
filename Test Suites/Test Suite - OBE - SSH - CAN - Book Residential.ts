52cc5da8d2e9c4efcf6737e02be3185d2b7adb4e
j_eag <justin.aguila@1800gotjunk.com>
2021-04-15 10:34:48 -0700

    ssh testing 4.15 commit

---
+++ Test Suites/Test Suite - OBE - SSH - CAN - Book Residential.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite - OBE - SSH - CAN - Book Residential</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a99a5370-b7eb-4ebf-acd2-b07f5251e08f</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2bab522-2f36-4aaf-b837-abd805ac5104</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/00.Web/01.OBE/TC_A-SSH-OBE03_CAN_Book_Residential</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ef6474e8-ec02-4ff3-83d4-de7ea74437bf</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data - WEB/Data - TC_A-SSH-OBE03_CAN_Book_Residential</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6939e170-d341-41be-916b-92b1cd2941e4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
